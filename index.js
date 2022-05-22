5adb41e7d72ec36620392dc55e0c78979becd4df
Nehemiah27 <96545668+Nehemiah27@users.noreply.github.com>
2022-05-22 22:48:22 +0530

    timerAdded

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-
+// Function of restart on button click
 function resetting() {
     alert("All your selections will be reset")
     window.location.reload(true)
 }
-
-setTimeout(() => {
-    alert("You are out of Time, Game will be restarted automatically\r\nYou Lost the Game\r\nGame Over!")
-    window.location.reload(true)
-}, 120000)
-
+// movesTaken variable used to comment the Player performance upon his successful close of game
 var movesTaken = 0;
+
+// function of movement of values to cab started
 function toCab(data) {
     var inLobbyOfficials = parseInt(document.getElementById("fo1").value);
     var inLobbyLuggage = parseInt(document.getElementById("fl1").value);
@@ -18,6 +15,8 @@ function toCab(data) {
 
     var userPickedOfficialsAtLobby = parseInt(document.getElementById("io1").value);
     var userPickedLuggageAtLobby = parseInt(document.getElementById("il1").value);
+
+    //     General check on the user inputs at Lobby side
     if (userPickedOfficialsAtLobby != 0 && userPickedOfficialsAtLobby != 1 && userPickedOfficialsAtLobby != 2) {
         alert("Please make the choice as per the Game rules")
     } else if (userPickedLuggageAtLobby != 0 && userPickedLuggageAtLobby != 1 && userPickedLuggageAtLobby != 2) {
@@ -29,6 +28,7 @@ function toCab(data) {
     } else if (userPickedLuggageAtLobby > inLobbyLuggage || userPickedOfficialsAtLobby > inLobbyOfficials) {
         alert("Please check your Selection\r\nYour Selection is out of available choices & Rules")
     }
+    //     actual action upon correct user inputs
     else if (data.movingToCab.onclick) {
         if (userPickedOfficialsAtLobby == 2 || userPickedOfficialsAtLobby == 1) {
             data.leftOfficial.value = inLobbyOfficials - userPickedOfficialsAtLobby;
@@ -38,16 +38,18 @@ function toCab(data) {
             data.leftLuggage.value = inLobbyLuggage - userPickedLuggageAtLobby;
             data.rightLuggage.value = inCabLuggage + userPickedLuggageAtLobby;
         }
+        // disabling the buttons after the action performed
 
-        document.getElementById("io1").disabled=true;
-        document.getElementById("il1").disabled=true;
-        document.getElementById("io2").disabled=false;
-        document.getElementById("il2").disabled=false;
-        
+        document.getElementById("io1").disabled = true;
+        document.getElementById("il1").disabled = true;
+        document.getElementById("io2").disabled = false;
+        document.getElementById("il2").disabled = false;
+
         document.getElementById("c").disabled = true;
         document.getElementById("l").disabled = false;
         movesTaken++;
 
+        //         clearing the user inputs after the action performed
         data.leftOI.value = 0;
         data.leftLI.value = 0;
         data.rightOI.value = 0;
@@ -58,6 +60,8 @@ function toCab(data) {
     var b = data.leftLuggage.value;
     var c = data.rightOfficial.value;
     var d = data.rightLuggage.value;
+
+    //     a check on the balance of rules of the game
     if (b != 0 && a > b) {
         alert("Officals got annoyed, Its an Unhappy Journey\r\nOfficials & Servant are now Helpless!\r\nGame Over!")
 
@@ -81,6 +85,10 @@ function toCab(data) {
     }
 }
 
+// function to the movement of values to cab ended 
+
+// function to the movement of values back to the lobby started
+
 function toLobby(data) {
     var inLobbyOfficials = parseInt(document.getElementById("fo1").value);
     var inLobbyLuggage = parseInt(document.getElementById("fl1").value);
@@ -89,6 +97,8 @@ function toLobby(data) {
 
     var userPickedOfficialsAtCab = parseInt(document.getElementById("io2").value);
     var userPickedLuggageAtCab = parseInt(document.getElementById("il2").value);
+
+    //     General check on the user inputs
     if (userPickedOfficialsAtCab != 0 && userPickedOfficialsAtCab != 1 && userPickedOfficialsAtCab != 2) {
         alert("Please make the choice as per the Game rules")
     } else if (userPickedLuggageAtCab != 0 && userPickedLuggageAtCab != 1 && userPickedLuggageAtCab != 2) {
@@ -100,6 +110,8 @@ function toLobby(data) {
     } else if (userPickedLuggageAtCab > inCabLuggage || userPickedOfficialsAtCab > inCabOfficials) {
         alert("Please check your Selection\r\nYour Selection is out of available choices & Rules")
     }
+
+    //     actual action upon correct user inputs
     else if (data.movingToCab.onclick) {
         if (userPickedOfficialsAtCab == 2 || userPickedOfficialsAtCab == 1) {
             data.rightOfficial.value = inCabOfficials - userPickedOfficialsAtCab;
@@ -109,13 +121,16 @@ function toLobby(data) {
             data.rightLuggage.value = inCabLuggage - userPickedLuggageAtCab;
             data.leftLuggage.value = userPickedLuggageAtCab + inLobbyLuggage;
         }
-        document.getElementById("io1").disabled=false;
-        document.getElementById("il1").disabled=false;
-        document.getElementById("io2").disabled=true;
-        document.getElementById("il2").disabled=true;
+
+        // disabling the buttons after the action performed
+        document.getElementById("io1").disabled = false;
+        document.getElementById("il1").disabled = false;
+        document.getElementById("io2").disabled = true;
+        document.getElementById("il2").disabled = true;
         document.getElementById("c").disabled = false;
         document.getElementById("l").disabled = true;
         movesTaken++;
+        //         clearing the user inputs after the action performed
 
         data.rightOI.value = 0;
         data.rightLI.value = 0;
@@ -127,6 +142,7 @@ function toLobby(data) {
     var b = data.leftLuggage.value;
     var c = data.rightOfficial.value;
     var d = data.rightLuggage.value;
+    //     a check on the balance of rules of the game
 
     if (b != 0 && a > b) {
         alert("Officals got annoyed, Its an Unhappy Journey\r\nOfficials & Servant are now Helpless!\r\nGame Over!")
@@ -141,3 +157,43 @@ function toLobby(data) {
         document.getElementById("l").disabled = true;
     }
 }
+
+// function to movement of values back to the lobby ended
+
+// timer function started
+
+var isStart = false;
+function startOf() {
+    isStart = true;
+    if (time == 120) {
+
+        document.getElementById("io1").disabled = false;
+        document.getElementById("il1").disabled = false;
+        document.getElementById("io2").disabled = true;
+        document.getElementById("il2").disabled = true;
+        document.getElementById("c").disabled = false;
+        document.getElementById("l").disabled = true;
+    }
+    setTimeout(() => {
+        alert("You are out of Time, Game will be restarted automatically\r\nYou Lost the Game\r\nGame Over!")
+        window.location.reload(true)
+    }, 120000)
+    if (time < 120) {
+        document.getElementById("warning").innerHTML = "You've Already Started the Game, Please Play the Game";
+
+        setTimeout(() => {
+            document.getElementById("warning").innerHTML = "";
+
+        }, 5000)
+
+
+    }
+}
+var time = 120;
+var timer = setInterval(() => {
+    if (isStart && time >= 0) {
+        document.querySelector(".minutes").innerHTML = parseInt(time / 60, 10);
+        document.querySelector(".seconds").innerHTML = parseInt(time % 60, 10)
+        time--
+    }
+}, 1000)
